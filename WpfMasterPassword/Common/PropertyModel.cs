cf17b6dfd1c1d03b5b777444df66d734acaafd7c
ms <mail@gomasch.de>
2015-08-18 00:09:29 +0200

    Getting started with WPF

---
+++ WpfMasterPassword/Common/PropertyModel.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace WpfMasterPassword.Common
+{
+    public class PropertyModel<T>: BindableBase
+    {
+        private T _Value;
+        public T Value
+        {
+            get
+            {
+                return _Value;
+            }
+            set
+            {
+                _Value = value;
+                OnPropertyChanged("Value");
+            }
+        }
+
+        public PropertyModel()
+        {
+        }
+
+        public PropertyModel(T defaultValue)
+        {
+            this._Value = defaultValue;
+        }
+    }
+
+    public class PropertyReadonlyModel<T> : BindableBase
+    {
+        private T _Value;
+        public T Value
+        {
+            get
+            {
+                return _Value;
+            }
+        }
+
+        public PropertyReadonlyModel()
+        {
+        }
+
+        public PropertyReadonlyModel(T defaultValue)
+        {
+            this._Value = defaultValue;
+        }
+
+        public void SetValue(T newValue)
+        {
+            _Value = newValue;
+            OnPropertyChanged("Value");
+        }
+    }
+
+    public class PropertyDelegateModel<T> : BindableBase
+    {
+        private Func<T> GetValue;
+        private Action<T> SetValue;
+        public T Value
+        {
+            get
+            {
+                return GetValue();
+            }
+            set
+            {
+                SetValue(value);
+                OnPropertyChanged("Value");
+            }
+        }
+
+        public PropertyDelegateModel(Func<T> getValue, Action<T> setValue)
+        {
+            if (null == getValue) throw new ArgumentException("getValue");
+            if (null == setValue) throw new ArgumentException("setValue");
+            GetValue = getValue;
+            SetValue = setValue;
+        }
+
+        public void RaiseOnPropertyChanged()
+        {
+            OnPropertyChanged(() => Value);
+        }
+    }
+
+    public class PropertyDelegateReadonlyModel<T> : BindableBase
+    {
+        private Func<T> GetValue;
+        public T Value
+        {
+            get
+            {
+                return GetValue();
+            }
+        }
+
+        public PropertyDelegateReadonlyModel(Func<T> getValue)
+        {
+            this.GetValue = getValue;
+        }
+
+        public void RaiseOnPropertyChanged()
+        {
+            OnPropertyChanged(() => Value);
+        }
+    }
+}
---
+++ WpfMasterPassword/ViewModel/DocumendViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfMasterPassword.Common;
+
+namespace WpfMasterPassword.ViewModel
+{
+    public class DocumendViewModel : BindableBase
+    {
+
+    }
+}
