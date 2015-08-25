fc56f5438712434981b93ecba35b9eb401d8a25b
ms <mail@gomasch.de>
2015-08-25 22:57:00 +0200

    cleaned up

--- MasterPasswordLib/Properties/AssemblyInfo.cs
+++ MasterPasswordLib/Properties/AssemblyInfo.cs
@@ -1,27 +1,17 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ("MasterPasswordLib")]
-[assembly: AssemblyDescription ("Library for Master Password Algorithms")]
-[assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("")]
-[assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("Martin Schmidt")]
-[assembly: AssemblyTrademark ("")]
-[assembly: AssemblyCulture ("")]
+[assembly: AssemblyTitle("MasterPasswordLib")]
+[assembly: AssemblyDescription("Library for Master Password Algorithms")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCompany("gomasch.de")]
+[assembly: AssemblyCopyright("Copyright © Martin Schmidt")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+[assembly: AssemblyVersion("1.01.00")]
 
-[assembly: AssemblyVersion ("1.01.00")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
 
