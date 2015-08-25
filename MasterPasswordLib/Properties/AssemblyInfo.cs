628cfe7156c6c4c1c1edeeab7481830950f718e4
ms <mail@gomasch.de>
2015-08-25 21:52:37 +0200

    renamed root directory

--- MasterPasswordLib/Properties/AssemblyInfo.cs
+++ MasterPasswordLib/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ("MonoMasterPasswordLib")]
-[assembly: AssemblyDescription ("")]
+[assembly: AssemblyTitle ("MasterPasswordLib")]
+[assembly: AssemblyDescription ("Library for Master Password Algorithms")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
 [assembly: AssemblyProduct ("")]
-[assembly: AssemblyCopyright ("martin")]
+[assembly: AssemblyCopyright ("Martin Schmidt")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion ("1.0.*")]
+[assembly: AssemblyVersion ("1.01.00")]
 
 // The following attributes are used to specify the signing key for the assembly,
 // if desired. See the Mono documentation for more information about signing.
