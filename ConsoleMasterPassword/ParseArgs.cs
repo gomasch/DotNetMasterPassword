4b5be03e52450c3af9cfb1259d4e246b2ba264c9
ms <mail@gomasch.de>
2015-08-25 22:55:18 +0200

    minimal cleanup

--- ConsoleMasterPassword/ParseArgs.cs
+++ ConsoleMasterPassword/ParseArgs.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleMasterPassword
+﻿namespace ConsoleMasterPassword
 {
     /// <summary>
     /// helper class to go through command line arguments
