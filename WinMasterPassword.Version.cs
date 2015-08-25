404d4a6e6a318425dc1bb167be69569d193932ab
ms <mail@gomasch.de>
2015-08-25 22:56:06 +0200

    shared version part

---
+++ WinMasterPassword.Version.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+
+// SHARED Version file
+// added as a link to the CLI and GUI Windows EXE
+
+// define in assembly: [assembly: AssemblyTitle("")]
+// define in assembly: [assembly: AssemblyDescription("")]
+// define in assembly: [assembly: AssemblyConfiguration("")]
+// define in assembly: [assembly: AssemblyProduct("")]
+[assembly: AssemblyCompany("gomasch.de")]
+[assembly: AssemblyCopyright("Copyright © Martin Schmidt")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("en")]
+
+// define in assembly: [assembly: ComVisible()]
+// define in assembly: [assembly: Guid()]
+
+[assembly: AssemblyVersion("1.01.00")]
+[assembly: AssemblyFileVersion("1.01.00")]
+[assembly: AssemblyInformationalVersion("1.01.00")] // "Product Version"
